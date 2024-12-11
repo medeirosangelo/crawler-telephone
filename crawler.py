63a72faed607db01428ee829b0160e0f1be2d348
Ângelo Medeiros <medeirosangelogabrieltrabalho@gmail.com>
2024-12-11 11:55:42 -0400

    Primeiro commit do projeto crawler

---
+++ crawler.py
@@ -0,0 +1,110 @@
+
+import re
+import requests
+from bs4 import BeautifulSoup
+import threading
+
+
+DOMINIO = "https://django-anuncios.solyd.com.br"
+URL_AUTOMOVEIS = "https://django-anuncios.solyd.com.br/automoveis/"
+LINKS = []
+TELEFONES = []
+
+
+
+def requisicao(url):
+    try:
+        resposta = requests.get(url)
+        if resposta.status_code == 200:
+            return resposta.text
+        else:
+            print("Erro ao fazer requisição")
+    except Exception as erro:
+        print("Erro ao fazer requisição")
+        print(erro)
+
+def parsing(resposta_html):
+    try:
+        soup = BeautifulSoup(resposta_html, 'html.parser')
+        return soup
+    except Exception as erro:
+        print("Erro ao fazer o parsing HTML")
+        print(erro)
+
+
+def encontrar_links(soup):
+    try:
+        cards_pai = soup.find("div", class_="ui three doubling link cards")
+        cards = cards_pai.find_all("a")
+    except:
+        print("Erro ao encontrar links")
+        return None
+
+    links = []
+    for card in cards:
+        try:
+            link =  card['href']
+            links.append(link)
+        except:
+            pass
+
+    return links
+
+def acessar_anuncio(link):
+    try:
+        resposta = requests.get(DOMINIO + link)
+        if resposta.status_code == 200:
+            return resposta.text
+        else:
+            print("Erro ao fazer requisição de busca de telefone")
+    except Exception as erro:
+        print("Erro ao fazer requisição de busca de telefone")
+        print(erro)
+
+def encontrar_telefone(soup):
+    try:
+        descricao = soup.find_all("div", class_="sixteen wide column")[2].p.get_text().strip()
+    except:
+        print("Erro ao encontrar descrição")
+        return None
+
+    regex = re.findall(r"\(?0?([1-9]{2})[ \-\.\)]{0,2}(9[ \-\.]?\d{4})[ \-\.]?(\d{4})", descricao)
+    if regex:
+        return regex
+    #print(regex)
+
+def descobrir_telefones():
+    while True:
+        try:
+            link_anuncio = LINKS.pop(0)
+        except:
+            return None
+
+        resposta_anuncio =  requisicao(DOMINIO + link_anuncio)
+        
+        if resposta_anuncio:
+            soup_anuncio = parsing(resposta_anuncio)
+            if soup_anuncio:
+                telefones = encontrar_telefone(soup_anuncio)
+                if telefones:
+                    for telefone in telefones:
+                        print("Telefone encontrado: ", telefone)
+                        TELEFONES.append(telefone)
+
+if __name__ == "__main__":
+    resposta_busca = requisicao(URL_AUTOMOVEIS)
+    if resposta_busca:
+        soup_busca = parsing(resposta_busca)
+        if soup_busca:
+            LINKS = encontrar_links(soup_busca)
+
+            threading_1 = threading.Thread(target=descobrir_telefones)
+            threading_2 = threading.Thread(target=descobrir_telefones)
+
+            threading_1.start()
+            threading_2.start()
+
+            threading_1.join()
+            threading_2.join()
+            
+            print(TELEFONES)
